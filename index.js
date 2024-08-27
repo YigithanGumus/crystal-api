466af474563eea37f360e00f92c7f45d335691c8
YigithanGumus <yigithangumuss@gmail.com>
2024-08-27 23:41:49 +0300

    Inital commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+dotenv.config();
+
+app.get("/pr", () => {
+  console.log("test is successfull");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "shop",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("başarılı"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(5000, () => {
+  console.log("API Aktif durumdadır!");
+});
---
+++ routes/cart.js
@@ -0,0 +1,65 @@
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+const Cart = require("../models/Cart");
+
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/",verifyTokenAndAuthorization,  async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
